faa7cfa26a977e51f41fd776084ebc56e163f447
yashmadavi897 <yashmadavi99@gmail.com>
2024-07-14 01:00:35 +0530

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,21 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
+import './globals.css
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Fusion AI",
   description: "AI Platform",
-};
+};  
 
 export default function RootLayout({
   children,
@@ -15,8 +23,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
-}
+    <ClerkProvider>
+      <html lang='en'>
+        <body>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  )
+  
+  
