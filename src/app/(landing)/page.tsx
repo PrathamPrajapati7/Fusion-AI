d49b01255548412bf2c04d18b3098120b52ccb6a
yashmadavi897 <yashmadavi99@gmail.com>
2024-07-25 23:50:47 +0530

    usual commit

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,8 +1,22 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 const LandingPage = () =>{
     return(
-        <div>
+        <><div>
             <h1>Landing Page (unprotected)</h1>
         </div>
+        <Link href="/sign-in">
+        <Button> Sign-in </Button>
+        </Link>
+        <Link href="/sign-up">
+        <Button> Sign-up </Button>
+        </Link>
+        
+        
+        
+        </>
+                
     );
 }
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,10 +27,9 @@ export default function RootLayout({
     <ClerkProvider>
     <html lang='en'>
       <body>
-        
         {children}
       </body>
     </html>
   </ClerkProvider>
   )
-}
+}              
