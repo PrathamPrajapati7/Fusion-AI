1c619bc82de8b59cb49dbeca7f38e8b18ed129d4
yashmadavi897 <yashmadavi99@gmail.com>
2024-07-25 23:50:47 +0530

    usual commit

--- src/app/(dashboard)/(routes)/dashboard/page.tsx
+++ src/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,6 +1,9 @@
+import {UserButton} from "@clerk/nextjs";
 
 export default function DashboardPage() {
   return (
-    <h1>DashBoard Page (Protected)</h1>
-  );
+    <><h1>DashBoard Page (Protected)</h1>
+    <div className=""><UserButton afterSignOutUrl="/" /></div>
+    </>
+  )
 }
