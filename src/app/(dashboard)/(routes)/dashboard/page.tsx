7f45ba9d809127fb69a1e60ac3b39cc78d4581a3
SuhasDhawale <suhasdhawale57@gmail.com>
2024-07-28 00:40:55 +0530

    added heading for the dashboard page

--- src/app/(dashboard)/(routes)/dashboard/page.tsx
+++ src/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,9 +1,14 @@
-
-
-export default function DashboardPage() {
+const DashboardPage = () => {
   return (
-    <><h1>DashBoard Page (Protected)</h1>
-   
-    </>
-  )
-}
+    <div>
+      <div className="mb-8 space-y-4">
+        <h2 className="text-2xl md:text-4xl font-bold text-center">
+          Explore the power of AI
+        </h2>
+        
+      </div>
+    </div>
+  );
+};
+
+export default DashboardPage;
