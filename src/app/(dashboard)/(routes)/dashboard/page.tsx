e877a5bab4a09b086714fc6cc91443c053df3707
SuhasDhawale <suhasdhawale57@gmail.com>
2024-07-28 02:28:47 +0530

    corrected the Heading

--- src/app/(dashboard)/(routes)/dashboard/page.tsx
+++ src/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -56,7 +56,7 @@ const DashboardPage = () => {
     <div>
       <div className="mb-8 space-y-4">
         <h2 className="text-2xl md:text-4xl font-bold text-center">
-          Explore the power of AI
+          Explore the power of Fusion AI
         </h2>
         <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
           Chat with the smartest AI - Experience the power of AI
