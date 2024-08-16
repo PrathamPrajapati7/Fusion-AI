36cabe19d8828df8a3fcfcb5e7d71ba6fb314d70
yashmadavi897 <yashmadavi99@gmail.com>
2024-08-16 14:06:55 +0530

    chatbots working

--- src/components/empty.tsx
+++ src/components/empty.tsx
@@ -1,7 +1,17 @@
-export const Empty = () =>{
-    return(
-        <div>
-           <p>helllo</p>
-        </div>
-    )
-}
+import Image from "next/image";
+
+type EmptyProps = {
+  label: string;
+};
+
+export const Empty = ({ label }: EmptyProps) => {
+  return (
+    <div className="h-full p-20 flex flex-col items-center">
+      <div className="relative h-72 w-72">
+        <Image src="/empty.png.jpg" alt="Empty" fill />
+      </div>
+
+      <p className="text-muted-foreground text-sm text-center">{label}</p>
+    </div>
+  );
+};
