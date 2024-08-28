5a9b11cf2c722404c6ee05d96e89e4763f5d96ae
YashrajBhoi7 <Yashrajbhoi07@gmail.com>
2024-08-29 00:37:02 +0530

    image generation module added

--- .next/types/app/(auth)/layout.ts
+++ .next/types/app/(auth)/layout.ts
@@ -1,4 +1,4 @@
-// File: C:\Users\prath\OneDrive\Desktop\Fusion-AI\src\app\(auth)\layout.tsx
+// File: C:\Users\yashr\OneDrive\Desktop\Fusion-AI\src\app\(auth)\layout.tsx
 import * as entry from '../../../../src/app/(auth)/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
