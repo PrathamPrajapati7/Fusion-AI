28db48f0cf51d660c2b91316e2d14f9250bbe57e
SuhasDhawale <suhasdhawale57@gmail.com>
2024-09-08 19:54:39 +0530

    minor changes

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -130,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("6da59e822857e325")
+/******/ 		__webpack_require__.h = () => ("e98dd2bda4b472e1")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
